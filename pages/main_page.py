91c6e1fba407dfe209daa8ea10c1fddfbde064f6
Phil <etozhephil@yandex.ru>
2022-08-10 20:50:22 +0300

    Убраны лишние и добавлены нужные для главной страницы функции

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,22 @@
 from stepik_finale_project.pages.base_page import BasePage
-from selenium.webdriver.common.by import By
+from .locators import BasePageLocators
+
+
+# класс для работы с главной страницей сайта
 class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
+
+    # перейти в корзину
+    def go_to_the_basket(self):
+        basket_button = self.browser.find_element(*BasePageLocators.VIEW_BASKET_BTN)
+        basket_button.click()
+
+    # переход на страницу с формой авторизации
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         login_link.click()
+
+    # проверка на наличие ссылки на страницу логина
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
