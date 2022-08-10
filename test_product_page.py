91af930c586c8c28332e90f60e5bcadc63172ce8
Phil <etozhephil@yandex.ru>
2022-08-10 20:34:02 +0300

    Была произведена группировка тестов

---
+++ test_product_page.py
@@ -0,0 +1,91 @@
+import time
+
+import pytest
+
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+
+
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_add_to_basket_button()
+#     page.add_item_to_basket_button()
+#     page.should_be_success_alert()
+#
+#
+
+#
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_add_to_basket_button()
+#     page.add_item_to_basket_button()
+#     page.success_alert_disappeared()
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        email = str(time.time()) + "@fakemail.org"
+        login_page.register_new_user(email, 'password234SUPER342')
+        login_page.should_be_authorized_user()
+        yield browser
+
+    def test_guest_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_success_alert()
+
+    def test_guest_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_to_basket_button()
+        page.add_item_to_basket_button()
+        page.should_item_in_basket()
+
+
+# def test_guest_should_see_login_link_on_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#
+#
+# def test_guest_can_go_to_login_page_from_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#     page.go_to_login_page()
+#     login_page = LoginPage(browser, browser.current_url)
+#     login_page.should_be_login_in_url()
+#
+#
+# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_the_basket()
+#     basket_page = BasketPage(browser, browser.current_url)
+#     basket_page.should_be_empty_basket_basket()
+#     basket_page.should_be_label_basket_is_empty()
